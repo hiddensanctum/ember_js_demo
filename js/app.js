0d696fedfd4f93d7638bc5f4cf74ad2dce2386fe
James Chuang <jameschuang13@gmail.com>
2013-11-18 00:35:27 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  this.resource('about');
+  this.resource('posts', function() {
+    this.resource('post', { path: ':post_id' });
+  });
+});
+
+App.PostsRoute = Ember.Route.extend({
+  model: function() {
+    return posts;
+  }
+});
+
+App.PostController = Ember.ObjectController.extend({
+  isEditing: false,
+
+  edit: function() {
+    this.set('isEditing', true);
+  },
+
+  doneEditing: function() {
+    this.set('isEditing', false);
+  }
+});
+
+var posts = [{
+  id: '1',
+  title: 'Ember Ember Ember',
+  author: { name: "James" },
+  date: new Date('11-17-2013'),
+  excerpt: 'Ember is cool',
+  body: 'I want to use ember with rails'
+}, {
+  id: '2',
+  title: 'Rails is Cooler',
+  author: { name: "James" },
+  date: new Date('11-17-2013'),
+  excerpt: 'This is a test post about rails',
+  body: 'They can coexist side by side if they wanted to but they choose to destroy each other'
+}];
